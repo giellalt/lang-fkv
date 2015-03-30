901921783ea6274607aaad679ea045b67d5c6081
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2015-03-30 09:21:33 +0000

    LexSub -> Err/Lex in testing and development scripts. Please check that the scripts are working as they should.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/fkv@110197 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/src/morphology/testverblemmas.sh
+++ test/src/morphology/testverblemmas.sh
@@ -3,8 +3,8 @@
 # Kommando når man er i fkv: sh test/src/morphology/testverblemmas.sh
 # Dette skriptet tester at nesten alle lemmaene i verbs.lexc kan genereres. De som ikke kan genereres, kopieres til missingverbLemmas.txt. 
 
-# Hent ut lemmaer, bortsett fra utkommmenterte (^\!),  dem med hardkoding for spesielle former (STRAYFORMS|ENDLEX), eller negasjonsverbet (+Neg, som ikke har infinitivform), LexSub (som blir filtrert bort fra normgenerator). Lemmaene lagres som verbs
-grep ";" $GTHOME/langs/fkv/src/morphology/stems/verbs.lexc | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|LexSub|\+Neg)' | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > verbs
+# Hent ut lemmaer, bortsett fra utkommmenterte (^\!),  dem med hardkoding for spesielle former (STRAYFORMS|ENDLEX), eller negasjonsverbet (+Neg, som ikke har infinitivform), Err/Lex (som blir filtrert bort fra normgenerator). Lemmaene lagres som verbs
+grep ";" $GTHOME/langs/fkv/src/morphology/stems/verbs.lexc | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|\+Err\/Lex|\+Neg)' | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > verbs
 
 # Generer infitivsformen av verblemmaene, fjern dem som ikke gir analyse (de har V+ i output), lagres som analverbs
 sed 's/$/+V+Inf/' verbs | $LOOKUP $GTHOME/langs/fkv/src/generator-gt-norm.xfst | cut -f2 | grep -v "V+" | grep -v "^$" | sort -u > analverbs 
