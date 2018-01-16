b3b92e9f33f60538a2346fb2397eb2663c8792c7
Sjur N Moshagen <sjurnm@mac.com>
2018-01-16 15:03:52 +0000

    All dialect yaml testing shell scripts updated to use the correct fst’s, and the Makefile has been updated to only run the dialect tests when dialect fst’s have been enabled.

--- test/src/run-gt-norm-var-genyaml-testcases.sh
+++ test/src/run-gt-norm-var-genyaml-testcases.sh
@@ -5,7 +5,7 @@
 # over by the test runner.
 
 ###### Variables: #######
-transducer=oahpa-gt-norm-dial_Var
+transducer=gt-norm-dial_Var
 halftest=gen # analysis test
 yaml_file_subdir=gt-norm-yamls
 
