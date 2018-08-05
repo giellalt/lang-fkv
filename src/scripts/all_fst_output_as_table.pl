56e5aabdd3d66193744877dddd500e1ed599bd79
Trond Trosterud <trond.trosterud@uit.no>
2018-08-05 17:58:12 +0000

    päivämäärä + käänteinen aakkostaminen.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/fkv@169337 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- src/scripts/all_fst_output_as_table.pl
+++ src/scripts/all_fst_output_as_table.pl
@@ -7,18 +7,24 @@ use utf8 ;
 # HOW TO HOW TO HOW TO HOW TO, 
 # standing in fkv (here with +N+Sg+Par): 
 
-# cat src/morphology/stems/nouns.lexc |grep ";"|tr '\!' '£'| cut -d"£" -f1|tr '[+ ]' ':' |grep -v '_pl'|cut -d":" -f1| sort|uniq |sed 's/$/+N+Sg+Par/'|lookup src/generator-gt-norm-dial_Por.xfst| grep ' '|perl src/scripts/all_fst_output_as_table.pl > doc/TestiSgPar.xml
+# cat src/morphology/stems/nouns.lexc |grep ";"|tr '\!' '£'| cut -d"£" -f1|tr '[+ ]' ':' |grep -v '_pl'|cut -d":" -f1| sort|uniq |sed 's/$/+N+Sg+Par/'|lookup src/generator-gt-norm-dial_Por.xfst| grep '\t'|rev|sort|rev|perl src/scripts/all_fst_output_as_table.pl > doc/testit/TestiSgPar.xml
 
 # Thereafter, write a hyperlink to TestiSgPar.html in doc/KvenDocumentation.jspwiki, e.g. under Test reports
 
-print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>																			  \n" ;
+
+@months = qw( tammikuun helmikuun maaliskuun huhtikuun toukokuun kesäkuun heinäkuun elokuun syyskuun lokakuun marraskuun joulukuun );
+#@days = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+$year = $year+1900;
+
+print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>  \n" ;
 print "<!DOCTYPE document PUBLIC \"-//APACHE//DTD Documentation V2.0//EN\" \"http://forrest.apache.org/dtd/document-v20.dtd\">\n" ;
-print "<document xmlns:xi=\"http://www.w3.org/2001/XInclude\" xml:lang=\"en\">												  \n" ;
-print "  <header>																										  \n"	  ;
-print "    <title>Generated forms</title>																			  \n"		  ;
-print "  </header>																									  \n"		  ;
-print "  <body>																										  \n"		  ;
-print "    <section>																								  \n"		  ;
+print "<document xmlns:xi=\"http://www.w3.org/2001/XInclude\" xml:lang=\"en\">  \n" ;
+print "  <header> \n"  ;
+print "    <title>Generoidut muodot ($months[$mon] $mday. päivänä $year, klo. $hour:$min)</title>  \n"  ;
+print "  </header> \n"  ;
+print "  <body>	  \n"  ;
+print "    <section><title/>  \n"   ;
 print "<table>\n" ;
 
 while ($line = <> ) {
@@ -30,7 +36,6 @@ while ($line = <> ) {
 }
 
 print "<\/table>\n" ;
-
 print "    </section>\n" ;
-print "  </body>	 \n" ;
-print "</document>	 \n" ;
+print "  </body> \n" ;
+print "</document> \n" ;
