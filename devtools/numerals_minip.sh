0566628892e191562033c29b9963d357b4500bb3
Trond Trosterud <trond.trosterud@uit.no>
2020-06-08 14:26:45 +0200

    generate is hfst, not hfstol

--- devtools/numerals_minip.sh
+++ devtools/numerals_minip.sh
@@ -26,7 +26,7 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-     echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-fkv/src/generator-gt-norm-dial_Por.hfstol
+     echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-fkv/src/generator-gt-norm-dial_Por.hfst
 #  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-fkv/src/generator-gt-norm-dial_Por.xfst
   # echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-fkv/src/generator-gt-norm.xfst
  done
