a348e42970d94ed68c96a52bea40fbb676276c76
Thomas Kjærstad <64013684+th0masbk@users.noreply.github.com>
2023-10-10 11:18:23 +0200

    Pron+Qu paradigm test

---
+++ devtools/qu_minip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in fkv:
+# sh devtools/prop_minip.sh prop_42_sur | dfkvNorm | less
+# sh devtools/prop_minip.sh Sirainen | dfkvNorm 
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/fst/stems/pronouns.lexc | grep $PATTERN | sed 's/% /%/g' | tr ' +' ':' | cut -d ':' -f1 | sed 's/%/% /g' | tr -d '%'>$L_FILE
+
+P_FILE="test/data/testquparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+#    echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-fkv/src/generator-gt-norm.xfst
+    echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-fkv/src/generator-gt-norm-dial_Por.hfstol
+#   #echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-fkv/src/generator-gt-norm.xfst
+#   echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-fkv/src/generator-gt-norm-dial_Por.xfst
+ done
+ rm -f $L_FILE
+done
+
