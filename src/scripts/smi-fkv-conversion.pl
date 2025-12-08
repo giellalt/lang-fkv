e7fdd97f65e10e076bf645cc20ecbb4b0ae19de5
Sjur N Moshagen <sjurnm@mac.com>
2025-12-08 11:35:27 +0200

    SMN ⇒ FKV in comments

--- src/scripts/smi-fkv-conversion.pl
+++ src/scripts/smi-fkv-conversion.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl -w
 #
-# smi-smn-conversion.pl
-# Convert names in the SMI propernoun lexicon to smn.
-# $Id: smi-smn-conversion.pl 91035 2014-03-24 22:56:01Z sjur $
+# smi-fkv-conversion.pl
+# Convert names in the SMI propernoun lexicon to FKV needs.
 
 use strict;
 use utf8;
